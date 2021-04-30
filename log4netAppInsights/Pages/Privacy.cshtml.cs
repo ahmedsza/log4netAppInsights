7c160996952e34ca55259ba5aeccb0bef2d2826a
ahmeds <ahmeds@ms.com>
2021-04-30 11:05:20 +0200

    Add project files.

---
+++ log4netAppInsights/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace log4netAppInsights.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("privacy information");
+            _logger.LogError("privacy error");
+        }
+    }
+}
